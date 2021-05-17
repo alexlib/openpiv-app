3396c85d004a28af686783d568487e6da51488e8
Alex <alex.liberzon@gmail.com>
2021-05-17 23:13:47 +0300

    fixed bug with rgb2gray

--- src/app.py
+++ src/app.py
@@ -8,64 +8,37 @@ import imageio
 import matplotlib.pyplot as plt
 
 
-# from const import CLASSES, COLORS
-# from settings import windef_settings
-
-# @st.cache
-# def process_images():
-#     windef.piv(windef_settings)
-#     return True
-
-
-# @st.cache
-# def plot_result(
-#     image, detections, confidence_threshold=DEFAULT_CONFIDENCE_THRESHOLD
-# ):
-#     # loop over the detections
-#     (h, w) = image.shape[:2]
-#     labels = []
-#     for i in np.arange(0, detections.shape[2]):
-#         confidence = detections[0, 0, i, 2]
-
-#         if confidence > confidence_threshold:
-#             # extract the index of the class label from the `detections`,
-#             # then compute the (x, y)-coordinates of the bounding box for
-#             # the object
-#             idx = int(detections[0, 0, i, 1])
-#             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-#             (startX, startY, endX, endY) = box.astype("int")
-
-#             # display the prediction
-#             label = f"{CLASSES[idx]}: {round(confidence * 100, 2)}%"
-#             labels.append(label)
-#             cv2.rectangle(image, (startX, startY), (endX, endY), COLORS[idx], 2)
-#             y = startY - 15 if startY - 15 > 15 else startY + 15
-#             cv2.putText(
-#                 image, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2
-#             )
-#     return image, labels
-
-
 st.title("PIV with OpenPIV")
 
-# gifurl = 'https://64.media.tumblr.com/3decdb9824c82cc625396d5162b9c72c/tumblr_ohqkj1wMvh1qckzoqo2_500.gifv'
-gifurl = 'https://64.media.tumblr.com/15d6395f97f2d12e32a764c4a17be406/699471e89e1d5634-11/s500x750/eabb9c7c1ea719d4b9889d8e0217a878ed3f7a3f.gifv'
-
+gifurl1 = 'https://64.media.tumblr.com/3decdb9824c82cc625396d5162b9c72c/tumblr_ohqkj1wMvh1qckzoqo2_500.gifv'
+gifurl2 = 'https://64.media.tumblr.com/15d6395f97f2d12e32a764c4a17be406/699471e89e1d5634-11/s500x750/eabb9c7c1ea719d4b9889d8e0217a878ed3f7a3f.gifv'
+gifurl3 = 'http://www.vsj.jp/~pivstd/gif/image04.gif'
 
 gifurl = st.text_input('URL link to GIF file with particle motion',
-value = gifurl)
+value = gifurl3)
 
+st.text('Nice examples:')
+st.write(gifurl1)
+st.write(gifurl2)
+st.write('http://www.vsj.jp/~pivstd/gif/image41.gif')
+
+# im = imageio.imread(imageio.core.urlopen(gifurl).read(), '.gif')
 im = imageio.get_reader(gifurl)
 st.image(gifurl)
 
 images = []
 for frame in im:
-    images.append(tools.rgb2gray(frame))
+    # st.write(frame.shape)
+    if frame.ndim > 2:
+        frame = tools.rgb2gray(frame)
+    images.append(frame)
 
 newgif = []
 
 frame_range = st.slider('Frames',value=(0,len(images)),min_value=0,max_value=len(images))
 # st.write(frame_range[0],frame_range[1])
+
+arrow_length = st.slider('Arrow length scaling',min_value=1,max_value=10)
 
 counter = frame_range[0]
 for I,J in zip(images[frame_range[0]:frame_range[1]-1],images[frame_range[0]+1:frame_range[1]]):
@@ -73,7 +46,7 @@ for I,J in zip(images[frame_range[0]:frame_range[1]-1],images[frame_range[0]+1:f
     fig, ax = plt.subplots()
     ax.text(20,20, str(counter), color='y')    
     ax.imshow(I, cmap='gray', alpha=0.8, origin="upper")
-    ax.quiver(x, y, u, v, scale=100, color="r")
+    ax.quiver(x, y, u, v, scale=10*arrow_length, color="y")
     ax.axis('off')
     fig.savefig('tmp.png')
     # st.pyplot(fig)
