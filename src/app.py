f1838ff98be2b78b0f8c900f0cf0cebc3287d012
Alex Liberzon <alexlib@users.noreply.github.com>
2022-10-12 11:44:02 +0000

    using sidebar and dictionary for choice

--- src/app.py
+++ src/app.py
@@ -7,28 +7,41 @@ import numpy as np
 import imageio
 import matplotlib.pyplot as plt
 
+gifurl4 = ''
+urls = {
+    'Birds 1':'https://64.media.tumblr.com/3decdb9824c82cc625396d5162b9c72c/tumblr_ohqkj1wMvh1qckzoqo2_500.gifv',
+    'Birds 2':'https://64.media.tumblr.com/15d6395f97f2d12e32a764c4a17be406/699471e89e1d5634-11/s500x750/eabb9c7c1ea719d4b9889d8e0217a878ed3f7a3f.gifv',
+    'PIV Standard Project':'http://www.vsj.jp/~pivstd/gif/image04.gif',
+    'Sneezing':'https://www.npr.org/assets/img/2016/08/23/sneeze-slomo.gif',
+    'User input': gifurl4,
+}
+
+st.sidebar.markdown("## Select GIF")
+st.sidebar.markdown("You can **select** the values to change the *chart*.")
+
+# Using object notation
+with st.sidebar:
+    choice = st.selectbox(
+        "Select the GIF",
+        urls.keys(),
+    )
+
+# # Using "with" notation
+# with st.sidebar:
+#     add_radio = st.radio(
+#         "Choose a shipping method",
+#         ("Standard (5-15 days)", "Express (2-5 days)")
+#     )
 
 st.title("PIV with OpenPIV")
 
-gifurl1 = 'https://64.media.tumblr.com/3decdb9824c82cc625396d5162b9c72c/tumblr_ohqkj1wMvh1qckzoqo2_500.gifv'
-gifurl2 = 'https://64.media.tumblr.com/15d6395f97f2d12e32a764c4a17be406/699471e89e1d5634-11/s500x750/eabb9c7c1ea719d4b9889d8e0217a878ed3f7a3f.gifv'
-gifurl3 = 'http://www.vsj.jp/~pivstd/gif/image04.gif'
-gifurl4 = 'https://www.npr.org/assets/img/2016/08/23/sneeze-slomo.gif'
+st.markdown('## Selected:')
+st.write(choice)
 
-gifurl = st.text_input('URL link to GIF file with particle motion',
-value = gifurl4)
-
-st.text('Nice examples of birds:')
-st.write(gifurl1)
-st.write(gifurl2)
-
-st.text('Standard PIV images project:')
-st.write('http://www.vsj.jp/~pivstd/gif/image41.gif')
-
-st.write('https://cdn-images.postach.io/7d04521b-03ce-4aaf-92e2-cbaaf790865e/856ccd90-67da-4eea-9e47-ea015b00ac7c/fdb97cbd-efd4-4405-bb9b-763bc83a16c8.gif')
-
-st.text('Sneezing:')
-st.write('https://www.npr.org/assets/img/2016/08/23/sneeze-slomo.gif')
+if choice == 'User input':
+    gifurl = st.text_input('URL link to GIF file with particle motion', value = 'https://')
+else:
+    gifurl = urls[choice]
 
 # im = imageio.imread(imageio.core.urlopen(gifurl).read(), '.gif')
 im = imageio.get_reader(gifurl)
@@ -52,7 +65,7 @@ counter = frame_range[0]
 for I,J in zip(images[frame_range[0]:frame_range[1]-1],images[frame_range[0]+1:frame_range[1]]):
     x,y,u,v = simple_piv(I,J,plot=False)
     fig, ax = plt.subplots()
-    ax.text(20,20, str(counter), color='y')    
+    ax.text(20,20, str(counter), color='y')
     ax.imshow(I, cmap='gray', alpha=0.8, origin="upper")
     ax.quiver(x, y, u, -v, scale=10*arrow_length, color="r")
     ax.axis('off')
@@ -82,13 +95,13 @@ st.image('tmp.gif')
 
 # if img_file_buffer is not None:
 #     windef_settings.filepath_images = './demo'
-#     windef_settings.frame_pattern_a = img_file_extension 
+#     windef_settings.frame_pattern_a = img_file_extension
 #     windef_settings.frame_pattern_b = None
 
 # else:
 #     # Format and Image Sequence
 #     windef_settings.filepath_images = './demo'
-#     windef_settings.frame_pattern_a = 'demo_b.bmp' 
+#     windef_settings.frame_pattern_a = 'demo_b.bmp'
 #     windef_settings.frame_pattern_b = 'demo_c.bmp'
 #     # demo_image = DEMO_IMAGE
 #     # image = np.array(Image.open(demo_image))
