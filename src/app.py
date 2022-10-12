45069c755a008c4827b6097cede28eda6ba94ce6
Alex Liberzon <alexlib@users.noreply.github.com>
2022-10-12 16:52:29 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ import numpy as np
 import imageio
 import matplotlib.pyplot as plt
 
-user_gifurl = 'https://imgur.com/a/HCZIKFz'
+user_gifurl = 'https://github.com/alexlib/openpiv-app/raw/master/demo/ezgif-1-d0c2ae0f68.gif'
 urls = {
     'Birds 1':'https://64.media.tumblr.com/3decdb9824c82cc625396d5162b9c72c/tumblr_ohqkj1wMvh1qckzoqo2_500.gifv',
     'Birds 2':'https://64.media.tumblr.com/15d6395f97f2d12e32a764c4a17be406/699471e89e1d5634-11/s500x750/eabb9c7c1ea719d4b9889d8e0217a878ed3f7a3f.gifv',
