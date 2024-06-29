88c3731d1f790aff21bf7a8689221b0fe242ca56
Alex Liberzon <alexlib@users.noreply.github.com>
2024-06-29 23:15:39 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -71,8 +71,9 @@ arrow_length = st.slider('Arrow length scaling',min_value=1,max_value=10,value=1
 newgif = []
 counter = frame_range[0]
 for I,J in zip(images[frame_range[0]:frame_range[1]-1],images[frame_range[0]+1:frame_range[1]]):
-    vel = pyprocess.extended_search_area_piv(
-        frame_a.astype(np.int32), frame_b.astype(np.int32), window_size=32,
+    vel = process.extended_search_area_piv(
+        I.astype(np.int32), J.astype(np.int32), 
+        window_size=32,
         search_area_size=64,
         overlap=8
     )
@@ -104,42 +105,3 @@ imageio.mimsave('tmp.gif', newgif)
 
 st.image('tmp.gif')
 
-# img_file_buffer = st.file_uploader("Upload a pair of images", type=["png", "jpg", "jpeg","tif","bmp"])
-# img_file_extension = st.text("Extension") # need here to allow user to select image extension
-# window_size = st.slider(
-#    "Interrogation Window Size", 8, 128, windef_settings.windowsizes[0], 8
-# )
-# confidence_threshold = st.slider(
-#     "Confidence threshold", 0.0, 1.0, DEFAULT_CONFIDENCE_THRESHOLD, 0.05
-# )
-# confidence_threshold = st.slider(
-#     "Confidence threshold", 0.0, 1.0, DEFAULT_CONFIDENCE_THRESHOLD, 0.05
-# )
-# confidence_threshold = st.slider(
-#     "Confidence threshold", 0.0, 1.0, DEFAULT_CONFIDENCE_THRESHOLD, 0.05
-# )
-
-# if img_file_buffer is not None:
-#     windef_settings.filepath_images = './demo'
-#     windef_settings.frame_pattern_a = img_file_extension
-#     windef_settings.frame_pattern_b = None
-
-# else:
-#     # Format and Image Sequence
-#     windef_settings.filepath_images = './demo'
-#     windef_settings.frame_pattern_a = 'demo_b.bmp'
-#     windef_settings.frame_pattern_b = 'demo_c.bmp'
-#     # demo_image = DEMO_IMAGE
-#     # image = np.array(Image.open(demo_image))
-
-# success = process_images()
-# fig,ax = plot_result()
-# image, labels = annotate_image(image, detections, confidence_threshold)
-
-# st.image(
-#     image, caption=f"Processed image", use_column_width=True,
-# )
-
-# st.write(labels)
-
-# %%
